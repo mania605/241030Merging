68a82aa8746af019d621052750217108028ca786
mania605 <kim.minsun83@gmail.com>
2024-10-27 20:44:13 +0900

    유튜브 하단이미지박스 및 반응형 1차

--- src/components/sub/Youtube.jsx
+++ src/components/sub/Youtube.jsx
@@ -12,26 +12,35 @@ export default function Youtube() {
 
 	const { data: Vids, isPending } = useYoutubeQuery({ type: 'B' });
 
+
 	return (
 		<Layout title={'YOUTUBE'}>
-			<Content delay={1}>
-				{isPending && <p>Loading...</p>}
-				{Vids?.map((vid, idx) => {  
-					return (
-						<article key={idx}>
-							<h3>
-								<Link to={'/youtube/' + vid.id}>{shortenText(vid.snippet.title, 60)}</Link>
-							</h3>
-							<div className='txt'>
-								<p>{shortenText(vid.snippet.description, 150)}</p>
-								<span>{combineText(vid.snippet.publishedAt.split('T')[0], '-', '.')}</span>
-							</div>
-							<Pic className='thumb' src={vid.snippet.thumbnails.high.url} />
-						</article>
-					);
-				})}
-			</Content>
+				<h1>AVALLION</h1>
+
+
+
+
+				<Content delay={1}>
+						{isPending && <p>Loading...</p>}
+						
+						{/* 비디오 카드를 8개까지만 렌더링 */}
+						<div className="video-grid">
+								{Vids?.slice(0, 8).map((vid, idx) => (
+										<article key={idx} className="video-card">
+										 	<p className="round">●</p>
+												<h3>
+													
+														<Link to={'/youtube/' + vid.id}>{shortenText(vid.snippet.title, 53)}</Link>
+												</h3>
+												<div className="txt">
+														<p>{shortenText(vid.snippet.description, 50)}</p>
+														<span>{combineText(vid.snippet.publishedAt.split('T')[0], '-', '.')}</span>
+												</div>
+												<Pic className='thumb' src={vid.snippet.thumbnails.high.url} />
+										</article>
+								))}
+						</div>
+				</Content>
 		</Layout>
-	);
+);
 }
- 
