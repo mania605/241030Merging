228e0e9200e8917ebc6e477b7fc4c3526025177d
mania605 <kim.minsun83@gmail.com>
2024-10-29 20:31:22 +0900

    유튜브 보더 바텀

--- src/components/common/ColorSelector.jsx
+++ src/components/common/ColorSelector.jsx
@@ -1,7 +1,7 @@
 export default function ColorSelector() {
-	const colors = ['40,40,40', '0,0,0', '255,255,255'];
+	const colors = ['40,40,40', '0,0,0', '200,200,200'];
 	const outerStyle = { display: 'flex', gap: 10, position: 'fixed', left: '5vw', bottom: '10vh', zIndex: 6 };
-	const btnStyle = { width: 14, height: 14, borderRadius: '50%', cursor: 'pointer', border: 'none' };
+	const btnStyle = { width: 14, height: 14, borderRadius: '50%',        border: '1px solid rgba(0, 0, 0, 0.5)',  backgroundColor: '#ffffff',cursor: 'pointer' };
 	const changeColor = color => {
 		document.documentElement.style.setProperty('--keyRGB', color);
 	};
